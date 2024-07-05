73d1914f334bafebc787dc157fe5f0be155ec3f2
nhatanhng <nguyennhatanh0810@gmail.com>
2024-07-05 23:53:09 +0700

    remove unnecessary file and codelines

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,16 +1,9 @@
 import React, {Suspense} from 'react'
 import { useRoutes } from 'react-router-dom'
-// import DefaultLayout from './Layouts/DefaultLayout'
 import UserLayout from './Layouts/UserLayout'
-
-
-
-// const LoginPage = React.lazy(() => import('./Login'))
-// const SignupPage = React.lazy(() => import('./Signup'))
 
 const HomePage = React.lazy(() => import('./Home'))
 const FilesPage = React.lazy(() => import('./UserPages/Files'))
-  // const FilesPage = React.lazy(() => import('./UserPages/File1'))
 const ImagesPage = React.lazy(() => import('./UserPages/Images'))
 const StatisticsPage = React.lazy(() => import('./UserPages/Statistics'))
 const AboutPage = React.lazy(() => import('./UserPages/About'))
@@ -28,19 +21,6 @@ export const LoadComponent = ({ component: Component }) => (
 
 const AllRoutes = () => {
   return useRoutes([
-    // {
-    //   element: <DefaultLayout />,
-    //   children: [
-    //     {
-    //       path: '/',
-    //       element: <LoadComponent component={LoginPage} />
-    //     },
-    //     {
-    //       path: '/register',
-    //       element: <LoadComponent component={SignupPage} />
-    //     }
-    //   ]
-    // },
     {
       path: '/',
       element: <LoadComponent component={HomePage}/>
