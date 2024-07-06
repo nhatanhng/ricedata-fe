d6e8b78225751c317afed20b0a2903cd98dfe1e3
nhatanhng <nguyennhatanh0810@gmail.com>
2024-07-06 14:04:10 +0700

    Update Images.jsx
    be able to display converting HSI images to whole page by choosing file from the dropdown

--- src/UserPages/Images.jsx
+++ src/UserPages/Images.jsx
@@ -1,69 +1,57 @@
-import React from 'react'
-import { Dropdown, Space, Button } from 'antd';
-import { DownOutlined, SmileOutlined } from '@ant-design/icons';
-import {  FileImageOutlined } from '@ant-design/icons' 
-
-
-const handleMenuClick = (e) => {
-  message.info('Click on menu item.');
-  console.log('click', e);
-}
-
-const items = [
-  {
-    key: '1',
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        1st menu item
-      </a>
-    ),
-  },
-  {
-    key: '2',
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        1st menu item
-      </a>
-    ),
-    
-  },
-  {
-    key: '3',
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        1st menu item
-      </a>
-    ),
-    disabled: true,
-  },
-  {
-    key: '4',
-    label: (
-      <a target="_blank" rel="noopener noreferrer">
-        1st menu item
-      </a>
-    ),
-  },
-];
-const menuProps = {
-  items,
-  onClick: handleMenuClick,
-};
-
-
+import React, { useState, useEffect } from 'react';
+import { Dropdown, Space, Button, message, Menu } from 'antd';
+import { FileImageOutlined } from '@ant-design/icons';
+import axios from 'axios';
 
 const Images = () => {
+  const [items, setItems] = useState([]);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  useEffect(() => {
+    // Fetch the list of visualized files from the server
+    axios.get('http://127.0.0.1:5000/visualized_files')
+      .then(response => {
+        const files = response.data;
+        const menuItems = files.map((file, index) => ({
+          key: index.toString(),
+          label: (
+            <a onClick={() => handleMenuClick(file)}>
+              {file}
+            </a>
+          ),
+        }));
+        setItems(menuItems);
+      })
+      .catch(error => {
+        message.error('Failed to fetch visualized files');
+      });
+  }, []);
+
+  const handleMenuClick = (file) => {
+    setSelectedImage(`http://127.0.0.1:5000/visualized/${file}`);
+  };
+
+  const menuProps = {
+    items,
+  };
+
   return (
-    <Dropdown menu={menuProps}
-    trigger={['click']}>
-    <Button>
-      <Space>
-        Images
-        <FileImageOutlined />
-      </Space>
-    </Button>
-  </Dropdown>
-  )
+    <div>
+      <Dropdown menu={menuProps} trigger={['click']}>
+        <Button>
+          <Space>
+            Images
+            <FileImageOutlined />
+          </Space>
+        </Button>
+      </Dropdown>
+      {selectedImage && (
+        <div style={{ marginTop: 20 }}>
+          <img src={selectedImage} alt="Selected" style={{ maxWidth: '100%' }} />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Images
+export default Images;
